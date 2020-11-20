f2d0c1b88c40b0c65acb773c8b9b8290a5e9eeaa
Aasha Bhandari <bhandariaasha700@gmail.com>
2020-11-20 13:00:53 +0545

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [repo, setRepo] = useState([]);
   const [error, setError] = useState(false);
   const [show, setShow] = useState(false);
- 
+
   function changeText(e) {
     setText(e.target.value);
   }
@@ -51,50 +51,51 @@ function App() {
       {error ? (
         <div>No User Found !</div>
       ) : (
-        <div className="profile-container">
-          <div className="left-info">
-            <img src={profile.avatar_url} className="profile_pic"></img>
-            <div className="name">{profile.name}</div>
-            <div className="username">{profile.login}</div>
-            <div className="bio">{profile.bio}</div>
-            <div className="location">
-              <img src={map} className="location-img" />
-              <div className="location-txt">{profile.location}</div>
+        profile.name && (
+          <div className="profile-container">
+            <div className="left-info">
+              <img src={profile.avatar_url} className="profile_pic"></img>
+              <div className="name">{profile.name}</div>
+              <div className="username">{profile.login}</div>
+              <div className="bio">{profile.bio}</div>
+              <div className="location">
+                <img src={map} className="location-img" />
+                <div className="location-txt">{profile.location}</div>
+              </div>
+              <div className="email">
+                <img src={mail} className="mail-img" />
+                <div className="mail-txt">{profile.email}</div>
+              </div>
+              <div className="follower">Followers {profile.followers}</div>
+              <div className="following">Following {profile.following}</div>
             </div>
-            <div className="email">
-              <img src={mail} className="mail-img" />
-              <div className="mail-txt">{profile.email}</div>
-            </div>
-            <div className="follower">Followers {profile.followers}</div>
-                  <div className="following">Following {profile.following}</div>
-          </div>
-          <div className="right-info">
-            <div>
-              <h3>Repositories</h3>
-            </div>
-            <div className="display">
-              {repo && repo.length ? (
-                repo.map((item) => {
-                  return (
-                    <div className="repos">
-                      <div className="name-container">
-                        <img src={bookmark} className="bookmark"></img>
-                        <div className="repo-names">
-                          <a href={item["html_url"]}>{item.name}</a>
+            <div className="right-info">
+              <div>
+                <h3>Repositories</h3>
+              </div>
+              <div className="display">
+                {repo && repo.length ? (
+                  repo.map((item) => {
+                    return (
+                      <div className="repos">
+                        <div className="name-container">
+                          <img src={bookmark} className="bookmark"></img>
+                          <div className="repo-names">
+                            <a href={item["html_url"]}>{item.name}</a>
+                          </div>
                         </div>
+                        <div className="desc">{item.description}</div>
+                        <div className="language">{item.language}</div>
                       </div>
-                      <div className="desc">{item.description}</div>
-                      <div className="language">{item.language}</div>
-                  
-                    </div>
-                  );
-                })
-              ) : (
-                <div>No Repositories Found !</div>
-              )}
+                    );
+                  })
+                ) : (
+                  <div>No Repositories Found !</div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        )
       )}
     </div>
   );
